7255b40c00351a8e5f58e93a874415106af25359
Jacob Garcia <324802@guhsd.net>
2017-12-04 21:26:28 +0000

    good boy strat added

--- jacob.py
+++ jacob.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Good Boy' # Only 10 chars displayed.
+strategy_name = 'kinda_nice_bot'
+strategy_description = 'it will collude on every fourth try if it has 600 extra points or if they just colluded'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,17 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+
+    if len(my_history) %4 == 0 and len(my_history) != 0:
+        if my_score - 600 > their_score:
+            return 'c'
+        else:
+            if their_history[-1] == 'c':
+                return 'c'
+            else:
+                return 'b'
+    else:
+        return 'b'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
