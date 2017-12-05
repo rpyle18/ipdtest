36e63da1c61155494818fa4d203e6a13e5a61811
Samantha Bell <306139@guhsd.net>
2017-12-05 21:06:08 +0000

    Adding code

--- sam.py
+++ sam.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Sam' # Only 10 chars displayed.
+strategy_name = 'Collude always unless betrayed then always betray.'
+strategy_description = 'It will always collude unless betryed by the other player'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -29,7 +29,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
