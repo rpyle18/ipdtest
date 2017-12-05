9870b3b8500ca3a131d740b66d7a06104658003f
Kyle Leja <323374@guhsd.net>
2017-12-05 21:04:45 +0000

    random

--- kyle.py
+++ kyle.py
@@ -8,8 +8,8 @@ import random
 ####
 
 team_name = 'Kyle' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Partially Random'
+strategy_description = 'If they have betrayed recently then retaliate, if not then randomly betryay 10% of the time'
     
 def move(my_history, their_history, my_score, their_score):
     if their_history[-10:] == 'b':
