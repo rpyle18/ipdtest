857ad54700ed222d1aa9471bf0c32b6c276c9587
Brayden Smith <323469@guhsd.net>
2017-12-05 21:14:11 +0000

    practice push

--- brayden.py
+++ brayden.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Me, Myself, And I' # Only 10 chars displayed.
+strategy_name = 'Tit fo tat'
+strategy_description = 'Do exactly as the oppenet?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
