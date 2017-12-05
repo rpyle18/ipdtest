959da6384d5436eaa3427ad6845a550e47f40bba
Ethan Neuhoff <323476@guhsd.net>
2017-12-05 21:42:38 +0000

    fixed errors in code

--- ethan.py
+++ ethan.py
@@ -19,11 +19,11 @@ def move(my_history, their_history, my_score, their_score):
     '''
     while 1 == 1:
         if their_history != 'c' and their_history != 'b':
-            move = 'b'
+            return 'b'
         elif their_history == 'c':
-            move = 'c'
+            return 'c'
         elif their_history == 'b':
-            move = 'b'
+            return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -32,7 +32,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return move
+  
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
