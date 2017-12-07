6d3f63fcd5a467739bf2b25a7c80cd918c44076c
Ethan Neuhoff <323476@guhsd.net>
2017-12-07 21:53:48 +0000

    fixed infinite loop

--- ethan.py
+++ ethan.py
@@ -17,15 +17,15 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    '''while 1 == 1:
-        if their_history != 'c' and their_history != 'b':
-            return 'b'
-        elif their_history == 'c':
-            return 'c'
-        elif their_history == 'b':
-
-            return 'b'
-'''
+    if their_history != 'c' and their_history != 'b':
+        return 'b'
+    elif their_history == 'c':
+        return 'c'
+    elif their_history == 'b':
+        return 'b'
+    elif their_history.count('c') == (my_history.count('c')-1):
+        return 'b'
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -35,7 +35,6 @@ def move(my_history, their_history, my_score, their_score):
     # Decide whether to return 'c' or 'b'.
     
 
-    return 'c'
 
 
     
