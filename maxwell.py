678593ad696633126d4697d0640fa75bcd1d0e6f
max godden <324697@guhsd.net>
2017-12-07 21:55:46 +0000

    added code

--- maxwell.py
+++ maxwell.py
@@ -25,6 +25,7 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    
     if their_history != 'b' and their_history != 'c':
         return 'b'
     if their_history[-1] == 'b':
