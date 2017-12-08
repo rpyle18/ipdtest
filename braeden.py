caf2f9953a7b437e5bb9dd1e20e1f92a4fc69155
Braeden Smith <306309@guhsd.net>
2017-12-08 21:38:28 +0000

    put in feeding code

--- braeden.py
+++ braeden.py
@@ -25,7 +25,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    return 'b'
+    while len(my_history) > -1:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
--- braeden2.py
+++ braeden2.py
@@ -11,23 +11,9 @@ strategy_name = 'Rat boys'
 strategy_description = 'Always betray'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     if len(my_history) == 0:
         return 'c'
-    if their_history(-1) == 'c':
+    if their_history[-1] == 'c':
         return 'b'
     else:
         return 'c'
