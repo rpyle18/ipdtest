d3b4dbd2da7aa98af0489cfc89e952d531d07971
ZACHARY STRANGE <305686@guhsd.net>
2017-12-08 21:12:02 +0000

    updating content / descriptions

--- zachary.py
+++ zachary.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Jake Paul' # Only 10 chars displayed.
+strategy_name = 'its everyday bro with the disney channel flow'
+strategy_description = 'Collude for 5 timmes then betray 5 times then collude 40 more times then betray the remaining 50'
     
 def move(my_history, their_history, my_score, their_score):
     if len(my_history) < 5:
@@ -38,7 +38,6 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
